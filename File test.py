03c16f858382cc1c863ae184064621c041ec57f5
KirillDavydov1 <82818161+KirillDavydov1@users.noreply.github.com>
2021-09-30 12:57:59 +0300

    Update File test.py

--- File test.py
+++ File test.py
@@ -4,24 +4,25 @@ import numpy
 import math
 import colorama as color
 
+
 def choice():
     inertialessUnitName = 'Безынерционное звено'
     aperiodUnitName = 'Апериодическое звено'
     integralUnitName = 'Интегральное звено'
     idealdiffUnitName = 'Идеальное дифференцирующее звено'
     realdiffUnitName = 'Реальное дифференцирующее звено'
-    
+
     needNewChoice = True
 
     while needNewChoice:
         print(color.Style.RESET_ALL)
         userInput = input('Введите номер команды: \n'
                           '1 - ' + inertialessUnitName + ';\n'
-                          '2 - ' + aperiodUnitName + ';\n'
-                          '3 - ' + integralUnitName + ';\n'
-                          '4 - ' + idealdiffUnitName + ';\n'
-                          '5 - ' + realdiffUnitName + '.\n')
-                          
+                                                         '2 - ' + aperiodUnitName + ';\n'
+                                                                                    '3 - ' + integralUnitName + ';\n'
+                                                                                                                '4 - ' + idealdiffUnitName + ';\n'
+                                                                                                                                             '5 - ' + realdiffUnitName + '.\n')
+
         if userInput.isdigit():
             needNewChoice = False
             userInput = int(userInput)
@@ -34,7 +35,7 @@ def choice():
             elif userInput == 4:
                 name = 'Идеальное дифференцирующее звено'
             elif userInput == 5:
-                name = 'Реальное дифференцирующее звено'    
+                name = 'Реальное дифференцирующее звено'
             else:
                 print(color.Fore.RED + '\nНедоступное значение')
                 needNewChoice = True
@@ -43,37 +44,33 @@ def choice():
             needNewChoice = True
     return name
 
+
 def getUnit(name):
     needNewChoice = True
     while needNewChoice:
         needNewChoice = False
-        k1 = input('Пожалуйста, введите коэффициент "k1" для безынерционного звена : ')
-        k2 = input('Пожалуйста, введите коэффициент "k2" для апериодического звена : ')
-        T2 = input('Пожалуйста, введите коэффициент "T2" для апериодического звена : ')
-        k3 = input('Пожалуйста, введите коэффициент "k3" для итегрального звена : ')
-        T3 = input('Пожалуйста, введите коэффициент "T3" для итегрального звена : ')
-        k4 = input('Пожалуйста, введите коэффициент "k4" для идеального дифференцирующего звена : ')
-        T4 = input('Пожалуйста, введите коэффициент "T4" для идеального дифференцирующего звена : ')
-        k5 = input('Пожалуйста, введите коэффициент "k5" для реального дифференцирующего звена : ')
-        T5 = input('Пожалуйста, введите коэффициент "T5" для реального дифференцирующего звена : ')
-        
-        if k1.isdigit() and T2.isdigit():
-            k1 = int(k1)
-            T2 = int(T2)
+
+        k = input('Пожалуйста, введите коэффициент "k": ')
+        T = input('Пожалуйста, введите коэффициент "T": ')
+
+        if k.isdigit() and T.isdigit():
+            k = int(k)
+            T = int(T)
             if name == 'Безынерционное звено':
-                unit = mlab.tf([k1], [1])
+                unit = mlab.tf([k], [1])
             elif name == 'Апериодическое звено':
-                unit = mlab.tf([k2], [T2, 1])
+                unit = mlab.tf([k], [T, 1])
             elif name == 'Интегральное звено':
-                unit = mlab.tf([k3], [])
+                unit = mlab.tf([1], [T, 0])
             elif name == 'Идеальное дифференцирующее звено':
-                unit = mlab.tf([k4], [])
+                unit = mlab.tf([T, 0], [1 / 100000, 1])
             elif name == 'Реальное дифференцирующее звено':
-                unit = mlab.tf([k5], [])    
+                unit = mlab.tf([k, 0], [T, 1])
         else:
             print(color.Fore.RED + '\nПожалуйста, введите числовое значение.\n')
             needNewChoice = True
     return unit
+
 
 def graph(num, title, y, x):
     mplplt.subplot(2, 1, num)
@@ -86,14 +83,36 @@ def graph(num, title, y, x):
     mplplt.ylabel('Амплитуда')
     mplplt.xlabel('Время (c)')
 
-unitName = choice()
-unit = getUnit(unitName)
 
-timeLine = []
-for i in range(0, 10000):
-    timeLine.append(i/1000)
-[y, x] = mlab.step(unit, timeLine)
-graph(1, 'Переходная характеристика', y, x)
-[y, x] = mlab.impulse(unit, timeLine)
-graph(2, 'Импульсная характеристика', y, x)
-mplplt.show()
+print('Введите "START" для выполнения программы, введите "EXIT" для завершения программы')
+PT = input()
+
+if PT == 'START':
+    running = True
+else:
+    running = False
+
+while running:
+
+    unitName = choice()
+    unit = getUnit(unitName)
+
+    timeLine = []
+    for i in range(0, 10000):
+        timeLine.append(i / 1000)
+    [y, x] = mlab.step(unit, timeLine)
+    graph(1, 'Переходная характеристика', y, x)
+    [y, x] = mlab.impulse(unit, timeLine)
+    graph(2, 'Импульсная характеристика', y, x)
+    mplplt.show()
+
+    mlab.bode(unit)
+    mplplt.plot()
+    mplplt.xlabel('Частота гц')
+    mplplt.show()
+    answer = input('Хотите ли вы продолжить? (Yes/No)')
+    if answer != 'Yes':
+        running = False
+
+else:
+    print('Программа завершена!')
